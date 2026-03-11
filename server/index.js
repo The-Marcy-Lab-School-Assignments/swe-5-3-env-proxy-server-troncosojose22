1ecf11ca398259e18c9f53b3269a8e2bdaf13aaf
troncosojose22 <troncosojose22@gmail.com>
2026-03-11 15:26:53 -0400

    added env file and safely stored API key

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@
 
 const path = require('path');
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 //////////////////////////
 // Constants
@@ -19,7 +21,25 @@ const app = express();
 
 const serveStatic = express.static(pathToFrontend);
 
+const serveGifs = async (req, res, next) => {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${process.env.API_KEY}`);
+        if (!response.ok) {
+            throw Error(`Fetch Failed: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        res.send(data);
+    }
+    catch(error) {
+        res.status(503).send(error);
+    }
+}
+
 app.use(serveStatic);
+
+app.get('/api/gifs', serveGifs);
 
 //////////////////////////
 // Listener
